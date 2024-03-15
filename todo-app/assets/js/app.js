9f0d27276d2e23b656b0d129a993fd2144d0679d
UP210052 <up210052@alumnos.upa.edu.mx>
2024-03-15 00:49:39 -0600

    Practica ToDo App

---
+++ todo-app/assets/js/app.js
@@ -0,0 +1,86 @@
+// Elementos HTML
+const userSelect = document.getElementById("select-users");
+const userContainer = document.getElementById("user-container");
+const taskContainer = document.getElementById("task-container");
+const buttonTask = document.getElementById("updateTask");
+let userName = document.getElementById("name");
+let userEmail =  document.getElementById("email");
+let userTask = document.getElementById("tasks");
+let userId = '';
+
+
+// Codígo nesesario para mostrar información
+
+userSelect.addEventListener("change", (evento) => {
+  getAllUsers().then((datos) => {
+    for (let i = 0; i < datos.length; i++) {
+      if(evento.target.value == i) {
+        // console.log(userContainer.children[1].children[0].children[0]);
+        userName.innerText = datos[i].firstname + " " + datos[i].lastname;
+        userEmail.innerText = datos[i].email;
+        userId = datos[i].id;
+        console.log(datos[i]);
+      }
+    }
+  });
+});
+
+buttonTask.addEventListener("click", () => {
+  getAllTasks().then((datos) => {
+    let task ='';
+    for (let i = 0; i < datos.length; i++) {
+      if(userId == datos[i].userId){
+        if(datos[i].completed) {
+          task += '<li> <span>'+ datos[i].title +'</span><input type="checkbox" checked> </li>';
+        } else {
+          task += '<li> <span>'+ datos[i].title +'</span><input type="checkbox" > </li>';
+        }
+        
+      }
+    }
+    console.log(task);
+    document.getElementById("tasks").innerHTML = task
+  });
+});
+
+
+
+
+// Fin de codígo
+
+// Funciones
+/**
+ * Optiene una lista de todos los usuarios que pueden existir
+ * @returns {Promise<User[]>}
+ */
+function getAllUsers() {
+  return fetch('/data/usuarios.json')
+    .then(resp => resp.json());
+}
+
+/**
+ * Optiene una lista de todas las tareas que hay de todos los usuarios
+ * @returns {Promise<Task[]>}
+ */
+function getAllTasks() {
+  return fetch('/data/tareas.json')
+    .then(resp => resp.json());
+}
+
+
+
+/**
+ * @typedef User Definición de un usuario
+ * @property {number} id Identificador unico del usuario
+ * @property {string} firstname Primer nombre del usuario
+ * @property {string} lastname Apellido del usuario
+ * @property {string} email Correo electronico del usuario
+ */
+
+/**
+ * @typedef Task Definición de una tarea de usuario
+ * @property {number} id Identificador unico de la tarea
+ * @property {number} userId IDentificador del uaurio a quien corresponde la tarea
+ * @property {string} title Titulo de la tarea
+ * @property {boolean} completed Estado de la tarea si esta completada o no
+ */
